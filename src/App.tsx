e7bab325d66dbfd87ec5fe655720255e6c563af2
joshhunt <josh@trtr.co>
2022-08-05 21:39:56 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ import React, {
   useState,
 } from "react";
 import "./App.css";
+import Select, {
+  components,
+  GroupBase,
+  MenuListProps,
+  MultiValue,
+  OptionProps,
+  SingleValue,
+} from "react-select";
 
 function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
@@ -40,46 +48,101 @@ for (let index = 0; index < randomShiftBy; index++) {
 const blockImageContext = require.context("./block-images/", false);
 const validBlockPaths = blockImageContext.keys();
 
+interface BlockOption {
+  label: string;
+  value: string;
+}
+
+const blockOptions: BlockOption[] = validBlockPaths.map((blockPath) => {
+  const blockId = blockPath.match(/\.\/(.*).png/)?.[1] ?? blockPath;
+
+  const blockName = blockId.replace(/_/g, " ");
+
+  return {
+    value: blockPath,
+    label: blockName,
+  };
+});
+
+interface BlockConfig {
+  blocks: BlockOption[];
+  chance: string;
+}
+
+function pick<T>(arr: T[]): T {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const initialBlockConfig = [
+  {
+    blocks: [
+      {
+        value: "./planks_spruce.png",
+        label: "planks spruce",
+      },
+    ],
+    chance: "",
+  },
+  {
+    blocks: [
+      {
+        value: "./log_spruce.png",
+        label: "log spruce",
+      },
+    ],
+    chance: "",
+  },
+  {
+    blocks: [
+      {
+        value: "./stripped_spruce_log.png",
+        label: "stripped spruce log",
+      },
+    ],
+    chance: "",
+  },
+  {
+    blocks: [
+      {
+        value: "./spruce_trapdoor.png",
+        label: "spruce trapdoor",
+      },
+    ],
+    chance: "",
+  },
+  {
+    blocks: [
+      {
+        value: "./log_spruce_top.png",
+        label: "log spruce top",
+      },
+    ],
+    chance: "",
+  },
+  {
+    blocks: [],
+    chance: "",
+  },
+];
+
 function App() {
   const widthId = useId();
   const heightId = useId();
-  const blocksId = useId();
-  const searchId = useId();
 
   const [width, setWidth] = useState(8);
   const [height, setHeight] = useState(5);
-  const [blocks, setBlocks] = useState<string[]>([
-    "log_spruce",
-    "log_spruce_top",
-    "planks_spruce",
-    "stripped_spruce_log",
-  ]);
-  const [searchText, setSearchText] = useState<string>("");
+  const [blockConfig, setBlockConfig] =
+    useState<BlockConfig[]>(initialBlockConfig);
 
-  const searchFilteredBlocks = useMemo(() => {
-    if (searchText.length === 0) {
-      return validBlockPaths;
-    }
-    const searchFilter = searchText.toLowerCase();
-    const searchFilterb = searchFilter.replace(/\s+/g, "_");
+  const [random, setRandom] = useState(0);
 
-    return validBlockPaths.filter((v) => {
-      const lower = v.toLowerCase();
-
-      return lower.includes(searchFilter) || lower.includes(searchFilterb);
-    });
-  }, [searchText]);
+  const reRandom = () => {
+    setRandom((v) => v + 1);
+  };
 
   const handleWidthChange = useCallback(
     (ev: React.ChangeEvent<HTMLInputElement>) => {
       setWidth(parseInt(ev.target.value) || 1);
-    },
-    []
-  );
-
-  const handleSearchChange = useCallback(
-    (ev: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchText(ev.target.value);
     },
     []
   );
@@ -91,36 +154,101 @@ function App() {
     []
   );
 
-  const handleBlocksChange = useCallback(
-    (ev: React.ChangeEvent<HTMLTextAreaElement>) => {
-      setBlocks(ev.target.value.split("\n").map((v) => v.trim()));
+  const { cleanedBlockConfig, autoChance } = useMemo(() => {
+    let cleanedBlockConfig = blockConfig
+      .map((v) => {
+        const chance = parseInt(v.chance);
+
+        return {
+          blocks: v.blocks.filter(Boolean),
+          chance: isNaN(chance) ? null : chance,
+        };
+      })
+      .filter((v) => v.blocks.length);
+
+    const assignedChance = cleanedBlockConfig.reduce(
+      (acc, config) => acc + (config.chance || 0),
+      0
+    );
+
+    const countOfAutoChance = cleanedBlockConfig.filter(
+      (v) => v.chance === null
+    ).length;
+
+    const autoChance = Math.floor(
+      Math.min(Math.max((100 - assignedChance) / countOfAutoChance, 0), 100)
+    );
+
+    return { cleanedBlockConfig, autoChance };
+  }, [blockConfig]);
+
+  const randomBlocks = useMemo(() => {
+    console.log(random);
+    const randomBlockOptions = cleanedBlockConfig.flatMap((blockConfig) => {
+      const chance = blockConfig.chance || autoChance;
+      const chancesForThisBlock: BlockOption[][] = new Array(
+        Math.floor(chance)
+      );
+      chancesForThisBlock.fill(blockConfig.blocks);
+      return chancesForThisBlock;
+    });
+
+    const render: BlockOption[][] = [];
+
+    for (let widthIndex = 0; widthIndex < width; widthIndex++) {
+      const row: BlockOption[] = [];
+
+      for (let heightIndex = 0; heightIndex < height; heightIndex++) {
+        const randomBlock = pick(pick(randomBlockOptions) ?? []);
+        if (randomBlock) {
+          row.push(randomBlock);
+        }
+      }
+
+      if (row.length) {
+        render.push(row);
+      }
+    }
+
+    return render;
+  }, [random, cleanedBlockConfig, autoChance, width, height]);
+
+  const handleSelectedBlockChange = useCallback(
+    (selection: MultiValue<BlockOption | undefined>, configIndex: number) => {
+      setBlockConfig((v) => {
+        const newConfig = [...v];
+
+        newConfig[configIndex] = {
+          blocks: selection.filter(Boolean) as BlockOption[],
+          chance: v[configIndex].chance,
+        };
+
+        const hasEmptyConfig = newConfig.some((v) => v.blocks.length === 0);
+        if (!hasEmptyConfig) {
+          newConfig.push({ blocks: [], chance: "" });
+        }
+
+        return newConfig;
+      });
     },
     []
   );
 
-  const randomBlocks = useMemo(() => {
-    const render: string[][] = [];
+  const handleBlockChanceChange = useCallback(
+    (ev: React.ChangeEvent<HTMLInputElement>, configIndex: number) => {
+      setBlockConfig((v) => {
+        const newConfig = [...v];
 
-    for (let widthIndex = 0; widthIndex < width; widthIndex++) {
-      const row: string[] = [];
+        newConfig[configIndex] = {
+          blocks: v[configIndex].blocks,
+          chance: ev.target.value,
+        };
 
-      for (let heightIndex = 0; heightIndex < height; heightIndex++) {
-        const randomBlock = blocks[Math.floor(Math.random() * blocks.length)];
-        row.push(randomBlock);
-      }
-
-      render.push(row);
-    }
-
-    return render;
-  }, [width, height, blocks]);
-
-  const addSearchResultToBlocks = useCallback((imageImportPath: string) => {
-    const match = imageImportPath.match(/\.\/(.*).png/);
-    if (match) {
-      setBlocks((v) => [...v, match[1]]);
-    }
-  }, []);
+        return newConfig;
+      });
+    },
+    []
+  );
 
   return (
     <div className="App">
@@ -131,10 +259,62 @@ function App() {
         <label htmlFor={heightId}>Height:</label>{" "}
         <input value={height} id={heightId} onChange={handleHeightChange} />
         <br />
-        <label htmlFor={blocksId}>Blocks (newline seperated):</label>
-        <br />
-        <textarea value={blocks.join("\n")} onChange={handleBlocksChange} />
+        <div className="block-config-grid">
+          <label>Blocks (newline seperated):</label>
+          <div>Chance</div>
+          <div />
+          {blockConfig.map((bc, configIndex) => {
+            const chancePlaceholder =
+              bc.blocks.length > 0 ? `${autoChance}%` : "auto";
+
+            return (
+              <>
+                <Select
+                  className="block-select"
+                  value={bc.blocks}
+                  isMulti
+                  options={blockOptions}
+                  onChange={(ev) => handleSelectedBlockChange(ev, configIndex)}
+                  formatOptionLabel={(option) => {
+                    const blockImage = validBlockPaths.includes(option.value)
+                      ? blockImageContext(option.value)
+                      : undefined;
+
+                    return (
+                      <div className="block-select-option">
+                        {blockImage && (
+                          <img
+                            alt=""
+                            src={blockImage}
+                            className="block-select-option-image"
+                          />
+                        )}
+                        <span>{option.label}</span>
+                      </div>
+                    );
+                  }}
+                />
+                <input
+                  type="text"
+                  value={bc.chance ?? ""}
+                  placeholder={chancePlaceholder}
+                  onChange={(ev) => handleBlockChanceChange(ev, configIndex)}
+                />
+                <div>
+                  {bc.chance.length
+                    ? bc.chance
+                    : bc.blocks.length > 0 && autoChance}
+                  %
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
+
+      <button onClick={reRandom}>re-random</button>
+      <br />
+      <br />
 
       <div
         className="block-grid"
@@ -147,12 +327,8 @@ function App() {
           return (
             <div className="blocks-row" key={index}>
               {row.map((block, blockIndex) => {
-                const blockColorIndex = blocks.indexOf(block);
-                const blockColor = COLORS[blockColorIndex];
-                const blockPath = `./${block}.png`;
-
-                const blockImage = validBlockPaths.includes(blockPath)
-                  ? blockImageContext(blockPath)
+                const blockImage = validBlockPaths.includes(block.value)
+                  ? blockImageContext(block.value)
                   : undefined;
 
                 return (
@@ -161,39 +337,15 @@ function App() {
                     key={blockIndex}
                     style={{
                       backgroundImage: `url("${blockImage}")`,
-                      backgroundColor: blockImage ? "transparent" : blockColor,
                     }}
                   >
-                    <span className="block-name">
-                      {block.replace(/_/g, " ")}
-                    </span>
+                    <span className="block-name">{block.label}</span>
                   </div>
                 );
               })}
             </div>
           );
         })}
-      </div>
-
-      <div>
-        <br />
-        <br />
-        <br />
-        <label htmlFor={searchId}>Search:</label>{" "}
-        <input value={searchText} id={searchId} onChange={handleSearchChange} />
-        <br />
-        <br />
-        <div className="search-grid">
-          {searchFilteredBlocks.map((v) => (
-            <img
-              key={v}
-              alt={v}
-              onClick={() => addSearchResultToBlocks(v)}
-              className="search-image"
-              src={blockImageContext(v)}
-            />
-          ))}
-        </div>
       </div>
     </div>
   );
